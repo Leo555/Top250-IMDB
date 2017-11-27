7b6abe0e5a326f0bfa2d6ae370131f323e175c13
LIZHEN2 <lizhen2@kingsoft.com>
2017-11-27 09:10:42 +0800

    bug fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Router from 'vue-router'
 import NProgress from 'nprogress'
 import { TITLE } from 'constants/index'
+import store from 'store'
 const Main = () => import('components/main/Index')
 const View = () => import('components/view/Index')
 const Search = () => import('components/view/Search')
 const NotFound = () => import('components/view/NotFound')
+const movies = store.state.movies
 
 NProgress.configure({
   showSpinner: false,
@@ -48,11 +50,16 @@ router.beforeEach((to, from, next) => {
   next()
 })
 
+const getMovieTitle = id => {
+  let movie = movies.find(m => m.id === id)
+  return movie.name
+}
+
 const updateTitle = to => {
   if (to.name === 'View') {
-    document.title = to.params.title || TITLE
+    document.title = to.params.title || getMovieTitle(to.params.id) || TITLE
   } else if (to.name === 'Search') {
-    document.title = `搜索：${to.params.keyword}- ${TITLE}`
+    document.title = `搜索：${decodeURIComponent(to.params.keyword)}- ${TITLE}`
   } else if (to.name === '404') {
     document.title = `404 - ${TITLE}`
   } else {
