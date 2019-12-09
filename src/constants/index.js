cb11b2a2dbed683aae5a4ea775de7c580f70541a
tickli <lizhen55521@gmail.com>
2019-12-09 22:04:21 +0800

    feat: 更新百度统计代码

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,4 +2,4 @@ export const PAGE_NUM = 16
 export const TITLE = 'IMDB Top250'
 export const KEYWORDS = 'IMDB, Movie, Movies, TOP250, 电影, IMDB电影, IMDBTOP250, 豆瓣, 下载, 电影下载'
 export const DESCRIPTION = '本网站提供IMDB电影评分前250电影简介和下载，内容全部来自网络，仅供学习参考，不得用于商业用途。'
-export const BAIDU_KEYS = 'b5f1ea8e9fea007ab688f73c2fc70aff'
+export const BAIDU_KEYS = '018cd2c2239b3d67796a17a6e03091c3'
